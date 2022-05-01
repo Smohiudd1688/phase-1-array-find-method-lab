112485b4926803f2c13a579df57e9952ccc0844d
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-04-30 21:17:29 -0700

    commit tree e667a54f0bab6aec76545feb7a7cc51a11faa724

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin (record) {
+    function isWin (element) {
+        if (element.result === 'W') {
+            return true;
+        }
+    }
+    
+    const result = record.find(isWin);
+    if (result === undefined) {
+        return result;
+    } else {
+        return result.year;
+    }
+}
+
